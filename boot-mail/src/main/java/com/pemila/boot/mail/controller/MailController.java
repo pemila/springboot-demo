5fab6e6a105681bca635ee489d0fa112e7d51d45
pemila <pemila@126.com>
2019-06-18 11:04:21 +0800

    add send mail with attachment

--- boot-mail/src/main/java/com/pemila/boot/mail/controller/MailController.java
+++ boot-mail/src/main/java/com/pemila/boot/mail/controller/MailController.java
@@ -2,6 +2,7 @@ package com.pemila.boot.mail.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 /**
  * @author 月在未央
@@ -26,6 +28,7 @@ public class MailController {
 
     private static final String TO = "1085824113@qq.com";
 
+    /** 发送简单邮件*/
     @GetMapping("/send/simpleMail")
     public String sendSimpleMail() {
         try {
@@ -42,6 +45,7 @@ public class MailController {
         }
     }
 
+    /** 发送html格式邮件*/
     @GetMapping("/send/htmlMail")
     public String sendHtmlMail(){
         try {
@@ -52,6 +56,27 @@ public class MailController {
             helper.setSubject("html邮件");
             // 带html格式的内容
             helper.setText("<p style='color:#42b983'>使用Spring Boot发送HTML格式邮件。</p>",true);
+            mailSender.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    /** 发送邮件带附件*/
+    @GetMapping("/send/mailWithAttach")
+    public String sendMailWithAttachment(){
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(TO);
+            helper.setSubject("带附件的邮件");
+            helper.setText("<p style='color:#42b983'>请查看附件</p>",true);
+            // 添加附件
+            File files = new File(ClassLoader.getSystemClassLoader().getResource("static/11.jpg").getPath());
+            helper.addAttachment("设计图标.jpg",files);
             mailSender.send(message);
             return "发送成功";
         } catch (Exception e) {
