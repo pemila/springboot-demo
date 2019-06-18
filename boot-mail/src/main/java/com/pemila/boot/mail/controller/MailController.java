2fbe8cf885cb48b51503a64d253348bce98e0669
pemila <pemila@126.com>
2019-06-18 10:38:09 +0800

    add send html mail

--- boot-mail/src/main/java/com/pemila/boot/mail/controller/MailController.java
+++ boot-mail/src/main/java/com/pemila/boot/mail/controller/MailController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.internet.MimeMessage;
 
 /**
  * @author 月在未央
@@ -21,15 +24,35 @@ public class MailController {
     @Value("${spring.mail.username}")
     private String from;
 
+    private static final String TO = "1085824113@qq.com";
+
     @GetMapping("/send/simpleMail")
     public String sendSimpleMail() {
         try {
             SimpleMailMessage mailMessage = new SimpleMailMessage();
             mailMessage.setFrom(from);
-            mailMessage.setTo("1085824113@qq.com");
+            mailMessage.setTo(TO);
             mailMessage.setSubject("邮件标题");
             mailMessage.setText("这是一封使用springboot发送的邮件");
             mailSender.send(mailMessage);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    @GetMapping("/send/htmlMail")
+    public String sendHtmlMail(){
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(TO);
+            helper.setSubject("html邮件");
+            // 带html格式的内容
+            helper.setText("<p style='color:#42b983'>使用Spring Boot发送HTML格式邮件。</p>",true);
+            mailSender.send(message);
             return "发送成功";
         } catch (Exception e) {
             e.printStackTrace();
